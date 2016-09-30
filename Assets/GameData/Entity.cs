21a96ce2d0c2a7de6340311fa75549ff2f1e9d71
Jleev77 <jleev77@gmail.com>
2016-09-29 22:05:08 -0400

    Added the start of the entity framework
    Added the start of the entity framework

---
+++ Assets/GameData/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.GameData
+{
+    public abstract class Entity : IEntity
+    {
+        public int ID { get; set; }
+        public GameObject GameObject { get; set; }
+    }
+}
