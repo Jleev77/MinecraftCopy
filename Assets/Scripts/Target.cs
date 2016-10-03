85dd31ae7dce583cc96277b0646855ab0798bbbf
Jleev77 <jleev77@gmail.com>
2016-10-03 17:59:01 -0400

    Added Targetting with events on targetchanged.  Also added debugger which uses it to display the the target name.
    Added Targetting with events on targetchanged.  Also added debugger
    which uses it to display the the target name.

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Target : MonoBehaviour {
+
+    public float MaxTargetDistance = 5f;
+    public Transform CurrentTarget { get; private set; }
+
+    private Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+        _camera = GetComponentInChildren<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 cameraCenter = _camera.ScreenToWorldPoint(new Vector3(Screen.width/2, Screen.height/2, _camera.nearClipPlane));
+        Ray ray = new Ray(cameraCenter, _camera.transform.forward);
+        RaycastHit hit = new RaycastHit();
+        Transform newTarget = Physics.Raycast(ray, out hit, MaxTargetDistance) ? hit.transform : null;
+
+        if(CurrentTarget != newTarget)
+        {
+            CurrentTarget = newTarget;
+
+            if(TargetChanged != null)
+            {
+                TargetChanged(this, new TargetChangedEventArgs(newTarget));
+            }
+        }
+	}
+
+    public delegate void TargetChangedEvent(object sender, TargetChangedEventArgs targetChangedEventArgs);
+    public event TargetChangedEvent TargetChanged;
+
+    public class TargetChangedEventArgs : EventArgs
+    {
+        public TargetChangedEventArgs(Transform newTarget)
+        {
+            NewTarget = newTarget;
+        }
+        public Transform NewTarget { get; protected set; }
+    }
+}
