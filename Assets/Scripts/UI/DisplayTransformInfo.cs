17069ed0217bcaeed49dbf378f30896d293c8f84
Jleev77 <jleev77@gmail.com>
2016-10-07 20:51:34 -0400

    Added player location to debugger UI
    Added player location to debugger UI

---
+++ Assets/Scripts/UI/DisplayTransformInfo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayTransformInfo : MonoBehaviour {
+
+    public Transform transformInfo;
+    private Text text;
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transformInfo.hasChanged)
+        {
+            Vector3 pos = transformInfo.position;
+            text.text = string.Format("{0}({1}, {2}, {3})", transformInfo.gameObject.name, pos.x, pos.y, pos.z);
+            Debug.Log("Hit");
+            transformInfo.hasChanged = false;
+        }
+	}
+}
